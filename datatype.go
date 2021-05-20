95983975e40f0c69d4eb3d415a885976def6b79d
stevenzack <zigzigcheers@gmail.com>
2021-05-20 10:53:45 +0800

    support-Time

--- datatype.go
+++ datatype.go
@@ -23,6 +23,11 @@ func ToDynamoDBType(t reflect.Type) (string, error) {
 		case reflect.Uint16, reflect.Uint32, reflect.Uint, reflect.Uint64, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int, reflect.Int64, reflect.Float32, reflect.Float64:
 			return "NS", nil
 		}
+	case reflect.Struct:
+		switch t.String() {
+		case "time.Time":
+			return "S", nil
+		}
 	}
 	return "", errors.New("unsupport field type:" + t.String() + ",kind=" + t.Kind().String())
 }
