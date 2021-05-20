9a2735cd2ee34233015163dba3c86f84b9cf639f
zhouyuhao <stevenzack@qq.com>
2021-05-20 13:05:46 +0800

    remove-createtable

--- basemodel.go
+++ basemodel.go
@@ -32,10 +32,6 @@ const (
 )
 
 func NewBaseModel(region string, data interface{}) (*BaseModel, error) {
-	model, _, e := NewBaseModelWithCreated(region, data)
-	return model, e
-}
-func NewBaseModelWithCreated(region string, data interface{}) (*BaseModel, bool, error) {
 	t := reflect.TypeOf(data)
 	b := &BaseModel{
 		Type:      t,
@@ -44,18 +40,18 @@ func NewBaseModelWithCreated(region string, data interface{}) (*BaseModel, bool,
 	}
 
 	if t.NumField() == 0 {
-		return nil, false, errors.New("Invalid struct [" + t.Name() + "] : no field in struct")
+		return nil, errors.New("Invalid struct [" + t.Name() + "] : no field in struct")
 	}
 	var e error
 	b.session, e = session.NewSession(aws.NewConfig().WithRegion(region))
 	if e != nil {
-		return nil, false, e
+		return nil, e
 	}
 	b.Client = dynamodb.New(b.session)
 
 	//check data
 	if t.Kind() == reflect.Ptr {
-		return nil, false, errors.New("data必须是非指针类型")
+		return nil, errors.New("data必须是非指针类型")
 	}
 
 	localIndexes := make(map[string][]*dynamodb.KeySchemaElement)
@@ -69,7 +65,7 @@ func NewBaseModelWithCreated(region string, data interface{}) (*BaseModel, bool,
 				reflect.Uint16,
 				reflect.String:
 			default:
-				return nil, false, errors.New("The first field " + field.Name + "'s type must be one of uint,uint32,uint64,uint16,string")
+				return nil, errors.New("The first field " + field.Name + "'s type must be one of uint,uint32,uint64,uint16,string")
 			}
 		}
 
@@ -90,7 +86,7 @@ func NewBaseModelWithCreated(region string, data interface{}) (*BaseModel, bool,
 		dnmType, e := ToDynamoDBType(field.Type)
 		if e != nil {
 			log.Println(e)
-			return nil, false, fmt.Errorf("Field %s:%w", field.Name, e)
+			return nil, fmt.Errorf("Field %s:%w", field.Name, e)
 		}
 		//index
 		if _, ok := field.Tag.Lookup(TAG_INDEX); ok {
@@ -115,65 +111,10 @@ func NewBaseModelWithCreated(region string, data interface{}) (*BaseModel, bool,
 	if e != nil {
 		if strings.HasPrefix(e.Error(), dynamodb.ErrCodeResourceNotFoundException) {
 			//create table
-			req := &dynamodb.CreateTableInput{
-				TableName:   &b.TableName,
-				BillingMode: aws.String("PAY_PER_REQUEST"),
-				KeySchema: []*dynamodb.KeySchemaElement{
-					{
-						AttributeName: aws.String(b.dbTags[0]),
-						KeyType:       aws.String("HASH"),
-					},
-				},
-				AttributeDefinitions: []*dynamodb.AttributeDefinition{
-					{
-						AttributeName: aws.String(b.dbTags[0]),
-						AttributeType: aws.String(b.dnmTypes[0]),
-					},
-				},
-			}
-
-			//secondary
-			if b.secondary > -1 {
-				req.KeySchema = append(req.KeySchema, &dynamodb.KeySchemaElement{
-					AttributeName: aws.String(b.dbTags[b.secondary]),
-					KeyType:       aws.String("RANGE"),
-				})
-				req.AttributeDefinitions = append(req.AttributeDefinitions, &dynamodb.AttributeDefinition{
-					AttributeName: aws.String(b.dbTags[b.secondary]),
-					AttributeType: aws.String(b.dnmTypes[b.secondary]),
-				})
-			}
-
-			_, e := b.Client.CreateTable(req)
-			if e != nil {
-				log.Println(e)
-				return nil, false, e
-			}
-
-			//create indexes
-			idxReq := &dynamodb.UpdateTableInput{
-				TableName: aws.String(b.TableName),
-			}
-			for _, key := range localIndexes {
-				idxReq.GlobalSecondaryIndexUpdates = append(idxReq.GlobalSecondaryIndexUpdates, &dynamodb.GlobalSecondaryIndexUpdate{
-					Create: &dynamodb.CreateGlobalSecondaryIndexAction{
-						IndexName: aws.String(ToIndexName(key)),
-						KeySchema: key,
-						Projection: &dynamodb.Projection{
-							ProjectionType: aws.String("ALL"),
-						},
-					},
-				})
-			}
-			_, e = b.Client.UpdateTable(idxReq)
-			if e != nil {
-				log.Println(e)
-				return nil, false, e
-			}
-			return b, true, nil
+			return nil, errors.New("Table " + b.TableName + " to be created")
 		}
 		log.Println(e)
-		return nil, false, e
+		return nil, e
 	}
 
 	//index check
@@ -183,48 +124,23 @@ func NewBaseModelWithCreated(region string, data interface{}) (*BaseModel, bool,
 	}
 	localIndexes = m
 
-	idxReq := &dynamodb.UpdateTableInput{
-		TableName: aws.String(b.TableName),
-	}
 	remoteIndexes := make(map[string]struct{})
 	//drop remote index
 	for _, idx := range tableInfo.Table.GlobalSecondaryIndexes {
 		remoteIndexes[*idx.IndexName] = struct{}{}
 		if _, ok := localIndexes[*idx.IndexName]; !ok {
 			log.Println("Remote index to be dropped: ", b.TableName+"."+*idx.IndexName)
-			idxReq.GlobalSecondaryIndexUpdates = append(idxReq.GlobalSecondaryIndexUpdates, &dynamodb.GlobalSecondaryIndexUpdate{
-				Delete: &dynamodb.DeleteGlobalSecondaryIndexAction{
-					IndexName: idx.IndexName,
-				},
-			})
 		}
 	}
 
 	//create index
-	for k, v := range localIndexes {
+	for k, _ := range localIndexes {
 		if _, ok := remoteIndexes[k]; !ok {
 			log.Println("Remote index to be created: ", b.TableName, ".", k)
-			idxReq.GlobalSecondaryIndexUpdates = append(idxReq.GlobalSecondaryIndexUpdates, &dynamodb.GlobalSecondaryIndexUpdate{
-				Create: &dynamodb.CreateGlobalSecondaryIndexAction{
-					IndexName: aws.String(k),
-					KeySchema: v,
-					Projection: &dynamodb.Projection{
-						ProjectionType: aws.String("ALL"),
-					},
-				},
-			})
 		}
 	}
 
-	//apply index changed
-	if len(idxReq.GlobalSecondaryIndexUpdates) > 0 {
-		_, e := b.Client.UpdateTable(idxReq)
-		if e != nil {
-			log.Println(e)
-			return nil, false, e
-		}
-	}
-	return b, false, nil
+	return b, nil
 }
 
 func (b *BaseModel) Insert(v interface{}) error {
