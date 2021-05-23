39f873b3e74e43bdce039d79eb7248b7c00a1a21
stevenzack <zigzigcheers@gmail.com>
2021-05-23 23:20:46 +0800

    addStringToSet-return

--- dynamodbx.go
+++ dynamodbx.go
@@ -16,21 +16,24 @@ func ToTableName(s string) string {
 	return strcase.ToLowerCamel(s)
 }
 
-func AddStringToSet(attributes map[string]*dynamodb.AttributeValue, k, v string) bool {
+func AddStringToSet(attributes map[string]*dynamodb.AttributeValue, k, v string) ([]string, bool) {
+	list := []string{}
 	av := attributes[k]
 	if av == nil {
 		av = &dynamodb.AttributeValue{}
 	} else if len(av.L) > 0 {
 		for _, item := range av.L {
 			if item.S != nil {
+				list = append(list, *item.S)
 				if *item.S == v {
-					return false
+					return nil, false
 				}
 			}
 		}
 	}
 
 	av.L = append(av.L, &dynamodb.AttributeValue{S: &v})
+	list = append(list, v)
 	attributes[k] = av
-	return true
+	return list, true
 }
